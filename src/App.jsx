ed446e8871a4b0a560a5811cf8174dd6492caf84
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-09-14 19:40:11 -0300

    Implement CartContext for global state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { CartProvider } from "./context/CartContext";
 import ProductList from "./components/ProductList";
 
 export default function App() {
     return (
-        <div className="bg-gray-100 min-h-screen">
-            <h1 className="font-bold py-6 text-3xl text-center">
-                Arthur's Store
-            </h1>
-            <ProductList />
-        </div>
+        <CartProvider>
+            <div className="bg-gray-100 min-h-screen">
+                <h1 className="font-bold py-6 text-3xl text-center">
+                    Arthur's Store
+                </h1>
+                <ProductList />
+            </div>
+        </CartProvider>
     )
 }
