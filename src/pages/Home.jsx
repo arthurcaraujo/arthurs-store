b538f018f370754e3edbbc27f3fef26078e413a8
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-09-15 18:31:44 -0300

    Implement product listing on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+
 export default function Home() {
+    const [products, setProducts] = useState([]);
+    const { addToCart } = useContext(CartContext);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
     return (
-        <div className="p-4">Home Page</div>
+        <article className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+            {products.map(product => (
+                <div key={product.id} className="border p-4 rounded shadow">
+                    <Link to={`/product/${product.id}`}>
+                        <img
+                            alt={product.title}
+                            className="h-40 mx-auto"
+                            src={product.image}
+                        />
+                        <h2 className="font-bold text-lg">
+                            {product.title}
+                        </h2>
+                        <p>${product.price}</p>
+                    </Link>
+                    <button
+                        className="bg-blue-500 mt-2 px-4
+                            py-2 rounded text-white"
+                        onClick={() => addToCart(product)}
+                    >
+                        Add to cart
+                    </button>
+                </div>
+            ))}
+        </article>
     )
 }
