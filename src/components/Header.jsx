b90becc0e3e419864d23f022c13939fd7f447787
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-09-18 18:05:07 -0300

    Implement Login page and update navigation bar to show user and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,43 @@
+import { AuthContext } from "../context/AuthContext";
+import { CartContext } from "../context/CartContext";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 
 export default function Header() {
+    const { cart } = useContext(CartContext);
+    const { user, logout } = useContext(AuthContext);
+
     return (
-        <header>
-            <h1>Arthur's Store</h1>
-            <nav className="bg-white shadow p-4 flex gap-4">
+        <header className="grid grid-cols-1 lg:grid-cols-[auto_auto]">
+            <h1 className="bg-green-600">Arthur's Store</h1>
+            <nav className="bg-white p-4 flex gap-4
+                shadow justify-between items-center">
                 <Link to="/">Home</Link>
-                <Link to="/cart">Cart</Link>
                 <Link to="/checkout">Checkout</Link>
-                <Link to="/login">Login</Link>
+                <Link to="/cart">Cart ({cart.length})</Link>
+                {user && <Link to="/orders">Orders</Link>}
+                {user ?
+                    <>
+                        <span className="mr-2">
+                            Hi, {user.email}
+                        </span>
+                        <button
+                            className="bg-red-600 px-3 py-1
+                                rounded text-white"
+                            onClick={logout}
+                        >
+                            Logout
+                        </button>
+                    </>
+                    :
+                    <Link
+                        className="bg-blue-500 px-3 py-1
+                            rounded text-white"
+                        to="/login"
+                    >
+                        Login
+                    </Link>
+                }
             </nav>
         </header>
     )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,25 @@
+import { AuthContext } from "../context/AuthContext";
+import { useState, useContext } from "react";
+
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const { login } = useContext(AuthContext);
+
     return (
-        <div className="p-4">Login Page</div>
+        <article className="p-4 max-w-md mx-auto">
+            <input
+                className="border mb-2 p-2 w-full"
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Enter you email address"
+                type="email"
+                value={email}
+            />
+            <button
+                className="bg-blue-500 px-4 py-2 text-white"
+                onClick={() => login(email)}
+            >
+                Login
+            </button>
+        </article>
     )
 }
