a2241ca1e76884aba62b9cdd63b2792d475a2e38
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-10-04 20:56:55 -0300

    Enhance the navbar with better spacing and responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,92 @@
 import { AuthContext } from "../context/AuthContext";
 import { CartContext } from "../context/CartContext";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import websiteLogo from "../assets/website-logo.png";
 
 export default function Header() {
     const { cart } = useContext(CartContext);
     const { user, logout } = useContext(AuthContext);
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        if (user) {
+            const atIndex = user.email.indexOf("@");
+            setUsername(user.email.substring(0, atIndex));
+        }
+    }, [user]);
 
     return (
-        <header className="grid grid-cols-1 lg:grid-cols-[auto_auto]">
-            <h1 className="bg-green-600">Arthur's Store</h1>
-            <nav className="bg-white p-4 flex gap-4
-                shadow justify-between items-center">
-                <Link to="/">Home</Link>
-                <Link to="/checkout">Checkout</Link>
-                <Link to="/cart">Cart ({cart.length})</Link>
-                {user && <Link to="/orders">Orders</Link>}
+        <header>
+            <nav
+                className="bg-gray-600 fixed gap-x-[0.5rem] gap-y-[0.25rem] grid
+                    grid-cols-[auto_auto_auto] grid-rows-[auto_auto] items-center justify-between
+                    justify-items-center p-[0.5rem] shadow-sm top-0 w-full
+                    sm:gap-0 sm:grid-cols-[12rem_12rem_6.5rem_5.5rem] sm:grid-rows-1 sm:justify-center sm:px-0
+                    md:grid-cols-[15rem_15rem_9rem_6rem]
+                    lg:grid-cols-[20rem_20rem_14rem_6rem] lg:py-[1rem]
+                    xl:grid-cols-[25rem_25rem_19rem_6rem]"
+            >
+                <a
+                    className="col-[1_/_2] font-[Nunito] gap-x-[0.3rem] gap-y-0 grid
+                        grid-cols-[2.5rem_auto] grid-rows-[1.4rem_1.1rem] group
+                        items-center justify-self-start leading-[1] no-underline row-[1_/_3] sm:justify-self-start"
+                    href="https://arthurcaraujo.github.io/"
+                >
+                    <img
+                        alt="Website logo"
+                        className="border-2 border-[#322] col-[1_/_2] duration-500 h-[2.5rem]
+                            group-active:border-[#ddd] group-hover:scale-[1.125] max-h-full
+                            rounded-full row-[1_/_3] w-[2.5rem] transition-[border, transform]"
+                        src={websiteLogo}
+                    />
+                    <span className="text-[1.4rem] text-shadow-[2px_2px_1px_#000] text-white">
+                        ARTHUR
+                    </span>
+                    <span className="italic text-[rgb(255,128,0)] text-[1.1rem]
+                        text-shadow-[2px_2px_1px_#000] whitespace-nowrap"
+                    >
+                        C. ARAÚJO
+                    </span>
+                </a>
+                <Link className="col-[2_/_3] row-[1_/_3]" to="/">
+                    <h1 className="bg-white font-[Asimovian] font-bold leading-none
+                            px-[0.4rem] py-[0.25rem] rounded-xl text-center text-orange-800
+                            [text-shadow:_1px_1px_1px_rgba(0,0,0)] text-2xl whitespace-nowrap lg:text-3xl"
+                    >
+                        Arthur's <br className="sm:hidden"/>Store
+                    </h1>
+                </Link>
+                <Link
+                    className="col-[3_/_4] font-bold row-[2_/_3] text-white
+                        sm:justify-self-end sm:row-[1_/_2]"
+                    to="/cart"
+                >
+                    Cart ({cart.length})
+                </Link>
                 {user ?
-                    <>
-                        <span className="mr-2">
-                            Hi, {user.email}
-                        </span>
-                        <button
-                            className="bg-red-600 px-3 py-1
-                                rounded text-white"
-                            onClick={logout}
-                        >
-                            Logout
-                        </button>
-                    </>
+                    <button
+                        className="bg-red-800 col-[3_/_4] px-[0.65rem] py-1 rounded row-[1_/_2] text-white
+                            sm:col-[4_/_5] sm:justify-self-end"
+                        onClick={logout}
+                    >
+                        Logout
+                    </button>
                     :
                     <Link
-                        className="bg-blue-500 px-3 py-1
-                            rounded text-white"
+                        className="bg-black col-[3_/_4] leading-none px-[0.65rem] py-[0.5rem]
+                            rounded-lg row-[1_/_2] text-white sm:col-[4_/_5] sm:justify-self-end"
                         to="/login"
                     >
                         Login
                     </Link>
                 }
             </nav>
+            <p className="mx-[0.5rem] pt-[5rem] sm:mx-auto sm:w-[36rem] md:w-[45rem] lg:w-[60rem] xl:w-[75rem]">
+                Welcome{user &&
+                    <span>, <Link className="active:text-red-700 font-bold hover:underline" to="/orders">{username}</Link></span>
+                }!
+            </p>
         </header>
     )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,38 +1,56 @@
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 
 export default function Cart() {
     const { cart, removeFromCart, clearCart } = useContext(CartContext);
 
+    const deleteItem = (item) => {
+        let confirmation = confirm("Are you sure you want to delete this item?");
+        confirmation && removeFromCart(item);
+    }
+    const deleteAll = () => {
+        let confirmation = confirm("Are you sure you want to clear your cart?");
+        confirmation && clearCart();
+    }
+
     return (
-    <div className="p-4">
-        <h2 className="text-xl font-bold mb-4">
-            Your Cart
-        </h2>
-        {cart.length === 0 
-            ? <p>Your cart is empty.</p>
-            : cart.map(item => (
-                <div key={item.id} className="mb-2 flex justify-between items-center">
-                    <span>{item.title}</span>
+        <article className="p-4">
+            <h2 className="text-xl font-bold mb-4">
+                Your Cart
+            </h2>
+            {cart.length === 0 
+                ? <p>Your cart is empty.</p>
+                : cart.map(item => (
+                    <div key={item.id} className="mb-2 flex justify-between items-center">
+                        <span>{item.title}</span>
+                        <button
+                            className="bg-red-500 text-white
+                            px-2 py-1 rounded"
+                            onClick={() => deleteItem(item.id)}
+                        >
+                            Remove
+                        </button>
+                    </div>
+                ))
+            }
+            {cart.length !== 0 &&
+                <div>
                     <button
-                        className="bg-red-500 text-white
-                        px-2 py-1 rounded"
-                        onClick={()=>removeFromCart(item.id)}
+                        className="bg-gray-600 px-2 py-1
+                            rounded text-white"
+                        onClick={() => deleteAll()}
                     >
-                        Remove
+                        Clear cart
                     </button>
+                    <Link className="bg-blue-500 block" to="/checkout">
+                        Checkout
+                    </Link>
                 </div>
-            ))
-        }
-        {cart.length !== 0 &&
-            <button
-                className="bg-gray-600 px-2 py-1
-                    rounded text-white"
-                onClick={() => clearCart()}
-            >
-                Clear cart
-            </button>
-        }
-    </div>
+            }
+            <Link className="bg-blue-500" to="/orders">
+                Your orders
+            </Link>
+        </article>
     )
 }
