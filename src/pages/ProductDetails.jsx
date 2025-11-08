0d5ec56b9c8e91ab674828da5effde827a573f46
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-11-08 20:35:20 -0300

    Add link to navigate back to Homepage from ProductDetails view

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 
@@ -22,8 +23,15 @@ export default function ProductDetails() {
             lg:gap-x-[2rem] lg:grid-cols-[26rem_32rem]
             xl:gap-x-[4rem] xl:grid-cols-[29rem_42rem]"
         >
+            <Link
+                className="mb-2 row-[1_/_2] text-blue-700 hover:text-blue-500
+                    text-sm underline md:col-[1_/_3] md:mb-4"
+                to="/"
+            >
+                Back to Homepage
+            </Link>
             <div className="bg-white border border-gray-200 col-[1_/_2] h-60 p-[1rem] rounded-xl
-                md:h-auto md:rounded-2xl md:row-[1_/_7] md:w-[100%]
+                md:h-auto md:rounded-2xl md:row-[2_/_8] md:w-[100%]
                 lg:p-[2rem] lg:rounded-3xl xl:p-[3rem] xl:rounded-4xl"
             >
                 <img
@@ -33,22 +41,22 @@ export default function ProductDetails() {
                 />
             </div>
             <h2 className="text-2xl font-bold mt-4 text-gray-800
-                    md:col-[2_/_3] md:row-[1_/_2]"
+                    md:col-[2_/_3] md:row-[2_/_3]"
             >
                 {product.title}
             </h2>
             <p className="font-semibold mt-2 text-blue-600 text-lg
-                    md:col-[2_/_3] md:row-[2_/_3]"
+                    md:col-[2_/_3] md:row-[3_/_4]"
             >
                 ${product.price}
             </p>
-            <p className="mt-2 md:col-[2_/_3] md:row-[3_/_4]">
+            <p className="mt-2 md:col-[2_/_3] md:row-[4_/_5]">
                 {product.description}
             </p>
             <button
                 className="bg-blue-500 hover:bg-blue-600 cursor-pointer
                     text-white px-4 py-2 mt-4 rounded-lg
-                    md:col-[2_/_3] md:row-[4_/_5]"
+                    md:col-[2_/_3] md:row-[5_/_6]"
                 onClick={()=>addToCart(product)}
             >
                 Add to Cart
