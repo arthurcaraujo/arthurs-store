f447cbba350583010b933d972a80fec609720edc
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-11-10 18:33:47 -0300

    Style Orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function Orders() {
@@ -9,7 +10,16 @@ export default function Orders() {
     }, []);
 
     return (
-        <article className="p-4">
+        <article className="w-[100%] sm:bg-gray-200 sm:p-4 sm:rounded-lg sm:w-[36rem]
+            md:w-[45rem] lg:rounded-xl lg:w-[60rem] xl:w-[75rem]"
+        >
+            <Link
+                className="inline-block mb-4 text-blue-700 text-sm
+                    hover:text-blue-500 underline lg:mb-6"
+                to="/"
+            >
+                Back to Homepage
+            </Link>
             <h2 className="font-bold mb-4 text-xl">
                 Your Orders
             </h2>
@@ -17,16 +27,24 @@ export default function Orders() {
                 ? <p>No previous orders.</p>
                 : orders.map(o => (
                     <div
-                        className="border mb-4 p-3 rounded"
+                        className="border border-gray-500 mb-4 p-3 rounded-lg"
                         key={o.id}
                     >
                         <p>Order ID: {o.id}</p>
-                        <p>Total: ${o.total.toFixed(2)}</p>
+                        <p>Total: <span className="font-bold">${o.total.toFixed(2)}</span></p>
                         <details>
-                            <summary>Items</summary>
+                            <summary className="cursor-pointer hover:underline">
+                                Items
+                            </summary>
                             {o.items.map(i => (
-                                <p key={i.id}>
-                                    {i.title} — ${i.price}
+                                <p
+                                    className="bg-gray-300 flex gap-x-3
+                                        items-center justify-between mb-2 px-2
+                                        py-1 rounded-lg md:px-4 md:py-2"
+                                    key={i.id}
+                                >
+                                    <span>{i.title}</span>
+                                    <span>${i.price.toFixed(2)}</span>
                                 </p>
                             ))}
                         </details>
