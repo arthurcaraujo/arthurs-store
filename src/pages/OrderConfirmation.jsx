7c78962a51c65d4a0a394401db0de1c08200359d
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-11-10 19:03:01 -0300

    Polish OrderConfirmation page

--- src/pages/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation.jsx
@@ -8,14 +8,16 @@ export default function OrderConfirmation() {
         <p className="p-4">No order found.</p>
 
     return (
-        <article className="p-4">
+        <article className="sm:bg-gray-200 sm:p-4 sm:rounded-lg sm:w-[36rem]
+            md:w-[45rem] lg:p-6 lg:rounded-xl lg:w-[60rem] xl:w-[75rem]"
+        >
             <h2 className="text-2xl font-bold mb-4">
                 Thank you!
             </h2>
             <p>Order ID: {order.id}</p>
-            <p>Total: ${order.total.toFixed(2)}</p>
+            <p>Total: <span className="font-bold">${order.total.toFixed(2)}</span></p>
             <Link
-                className="block mt-4 text-blue-500 underline"
+                className="inline-block mt-4 text-blue-700 hover:text-blue-500 underline"
                 to="/orders"
             >
                 View All Orders
